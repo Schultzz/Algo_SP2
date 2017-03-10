bbcb79bd015b03ed32f57e7713a277fd7b7cda43
Schultzz <ms.fuge@gmail.com>
2017-03-10 19:13:05 +0100

    init

---
+++ Algo_SP2/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo_SP2
+{
+    class Program
+    {
+        private Stopwatch stopwatch;
+        private Random randomNumber;
+
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.Runner();
+        }
+
+        public TimeSpan TimeInsertionSort(int[] dataSet)
+        {
+            stopwatch = new Stopwatch();
+            stopwatch.Start();
+            SortingAlgoritms.InsertionSort(dataSet);
+            stopwatch.Stop();
+            return stopwatch.Elapsed;
+        }
+
+        public TimeSpan TimeSelectionSort(int[] dataSet)
+        {
+            stopwatch = new Stopwatch();
+            stopwatch.Start();
+            SortingAlgoritms.Selectsort(dataSet);
+            stopwatch.Stop();
+            return stopwatch.Elapsed;
+        }
+
+        public TimeSpan TimeMergeSort(int[] dataSet)
+        {
+            stopwatch = new Stopwatch();
+            var dataList = dataSet.ToList();
+            stopwatch.Start();
+            SortingAlgoritms.MergeSort(dataList);
+            stopwatch.Stop();
+            return stopwatch.Elapsed;
+        }
+
+        public int[] GenerateRandomArr(int n)
+        {
+            int[] randomNum = new int[n];
+            randomNumber = new Random();
+
+
+            for (int i = 0; i < n; i++)
+            {
+                randomNum[i] = randomNumber.Next(1, n);
+            }
+
+            return randomNum;
+        }
+
+        public void Runner()
+        {
+            var insertionSortVal = TimeInsertionSort(GenerateRandomArr(1000)).TotalSeconds;
+            var selectionSortVal = TimeInsertionSort(GenerateRandomArr(1000)).TotalSeconds;
+            var mergeSortVal = TimeInsertionSort(GenerateRandomArr(1000)).TotalSeconds;
+            for (int i = 2000; i <= 32000; i = i*2)
+            {
+                insertionSortVal *= 4;
+                Console.WriteLine("InsertionSort  i = " + i + " , " + "expected: " + insertionSortVal + " , actual: " +
+                                  TimeInsertionSort(GenerateRandomArr(i)));
+                selectionSortVal *= 4;
+                Console.WriteLine("SelectionSort  i = " + i + " , " + "expected: " + selectionSortVal + " , actual: " +
+                                  TimeSelectionSort(GenerateRandomArr(i)));
+                mergeSortVal *= 2;
+                Console.WriteLine("MergeSort      i = " + i + " , " + "expected: " + mergeSortVal + " , actual: " +
+                                  TimeMergeSort(GenerateRandomArr(i)));
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Algo_SP2/SortingAlgoritms.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Algo_SP2
+{
+    public class SortingAlgoritms
+    {
+        public static void InsertionSort(int[] inputArray)
+        {
+            for (int i = 0; i < inputArray.Length - 1; i++)
+            {
+                for (int j = i + 1; j > 0; j--)
+                {
+                    if (inputArray[j - 1] > inputArray[j])
+                    {
+                        int temp = inputArray[j - 1];
+                        inputArray[j - 1] = inputArray[j];
+                        inputArray[j] = temp;
+                    }
+                }
+            }
+        }
+
+
+        public static void Selectsort(int[] inputArray)
+        {
+            int i, j;
+            for (i = 0; i < inputArray.Length; i++)
+            {
+                int min = i;
+                for (j = i + 1; j < inputArray.Length - 1; j++)
+                    if (inputArray[j] < inputArray[min]) min = j;
+                int temp = inputArray[i];
+                inputArray[i] = inputArray[min];
+                inputArray[min] = temp;
+            }
+        }
+
+        public static List<int> MergeSort(List<int> unsorted)
+        {
+            if (unsorted.Count <= 1)
+                return unsorted;
+
+            List<int> left = new List<int>();
+            List<int> right = new List<int>();
+
+            int middle = unsorted.Count / 2;
+            for (int i = 0; i < middle; i++)  //Dividing the unsorted list
+            {
+                left.Add(unsorted[i]);
+            }
+            for (int i = middle; i < unsorted.Count; i++)
+            {
+                right.Add(unsorted[i]);
+            }
+
+            left = MergeSort(left);
+            right = MergeSort(right);
+            return Merge(left, right);
+        }
+
+        private static List<int> Merge(List<int> left, List<int> right)
+        {
+            List<int> result = new List<int>();
+
+            while (left.Count > 0 || right.Count > 0)
+            {
+                if (left.Count > 0 && right.Count > 0)
+                {
+                    if (left.First() <= right.First())  //Comparing First two elements to see which is smaller
+                    {
+                        result.Add(left.First());
+                        left.Remove(left.First());      //Rest of the list minus the first element
+                    }
+                    else
+                    {
+                        result.Add(right.First());
+                        right.Remove(right.First());
+                    }
+                }
+                else if (left.Count > 0)
+                {
+                    result.Add(left.First());
+                    left.Remove(left.First());
+                }
+                else if (right.Count > 0)
+                {
+                    result.Add(right.First());
+                    right.Remove(right.First());
+                }
+            }
+            return result;
+        }
+
+    }
+}
